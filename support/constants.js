70a144d366946e6e9e146c8e4d312ca4c40d8e7b
munishasharma <41514870+munishasharma@users.noreply.github.com>
2021-04-23 11:29:01 +0200

    Update constants.js
    For different devices

--- support/constants.js
+++ support/constants.js
@@ -1,2 +1,2 @@
-//export const sizes = ['iphone-x',[1280,800], [375,667]]
-export const sizes = ['iphone-x']
+export const sizes = ['iphone-x',[1280,800], [375,667]]
+//export const sizes = ['iphone-x']
